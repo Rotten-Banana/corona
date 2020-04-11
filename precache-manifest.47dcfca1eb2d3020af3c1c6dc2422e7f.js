e6ba876b2ebe879b6f9fa87df42eb58bfbff6db0
Souvik Srimani <55970375+Rotten-Banana@users.noreply.github.com>
2020-04-11 14:38:56 +0530

    Updates

---
+++ precache-manifest.47dcfca1eb2d3020af3c1c6dc2422e7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963d601194ab726bef830a86b3974a4c",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "6505ab6edffaeace8be4",
+    "url": "/corona/static/css/main.9f9e6e08.chunk.css"
+  },
+  {
+    "revision": "ab3a20af2fc3ce639d4b",
+    "url": "/corona/static/js/2.254a2d1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona/static/js/2.254a2d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6505ab6edffaeace8be4",
+    "url": "/corona/static/js/main.0afa59eb.chunk.js"
+  },
+  {
+    "revision": "10563266cc97041c2017",
+    "url": "/corona/static/js/runtime-main.b4d4afbe.js"
+  }
+]);
